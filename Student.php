acdefe1caae98da113860d8dc5c452ddcd1971db
Uitiam <maitiumorton@gmail.com>
2017-01-14 22:11:31 -0800

    added average function that calculates grade average

--- Student.php
+++ Student.php
@@ -16,5 +16,12 @@ class Student {
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
+
+    function average() {
+    $total = 0;
+    foreach ($this->grades as $value)
+        $total += $value;
+    return $total / count($this->grades);
+    }
 }
 ?>
